3c871f5dd21d5ecf65c7a9a9d09153936ca52348
Saikiran <saikithati83@gmail.com>
2022-01-22 15:52:00 +0530

    substr

---
+++ STRINGS/3find_rfind_index.py
@@ -0,0 +1,5 @@
+#string.find(substring,bEgin,end)
+#r find- finds from backward direction
+#index() method same like find, but if substr not there we'll get value error
+s="saikiran needs to learn a lot"
+print(s.find('nee'))
