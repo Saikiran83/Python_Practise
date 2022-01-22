9d663cb030e54a0476d7ca8cf908d42e2a748d1f
Saikiran <saikithati83@gmail.com>
2022-01-22 15:47:30 +0530

    changes made

---
+++ STRINGS/2strips.py
@@ -0,0 +1,7 @@
+#strip remove spaces in string
+#rstrip()  To remove spaces at right hand side
+# lstrip() To remove spaces at left hand side
+# strip()  To remove spaces both sides
+s="saikiran "
+s.strip()
+print(s)
