a289449e75ad6c7edffda35a12f2d4a1c4bc78c5
Saikiran <saikithati83@gmail.com>
2022-01-22 16:20:16 +0530

    strings

---
+++ STRINGS/5casechange.py
@@ -0,0 +1,14 @@
+#upper()  To convert all characters to upper case
+# lower()  To convert all characters to lower case
+# swapcase()  Converts all lower case characters to upper case and all upper case 
+#characters to lower case
+# title()  To convert all character to title case. i.e first character in every word should 
+#be upper case and all remaining characters should be in lower case.
+# capitalize()  Only first character will be converted to upper case and all remaining 
+#characters can be converted to lower case 
+s = 'learning Python is very Easy' 
+print(s.upper()) 
+print(s.lower()) 
+print(s.swapcase()) 
+print(s.title()) 
+print(s.capitalize())
