791cdcc458a2c4083b8dacd43ea08c1798613eae
Saikiran <saikithati83@gmail.com>
2022-01-26 00:33:49 +0530

    modules

---
+++ modules/1intro.py
@@ -0,0 +1,11 @@
+#Various Possibilties of import:
+#1) import modulename
+#2) import module1,module2,module3
+#3) import module1 as m
+#4) import module1 as m1,module2 as m2,module3
+#5) from module import member
+#6) from module import member1,member2,memebr3
+#7) from module import memeber1 as x
+#8) from module import * (no need to use math.sum() >directly use sum())
+
+#dir(moduleName)  To list out all members of specified module (all the fns in module)
