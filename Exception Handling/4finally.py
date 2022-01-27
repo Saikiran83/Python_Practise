6a5c070acc3eb31858c6bc2fb0765f544d99b912
Saikiran <saikithati83@gmail.com>
2022-01-27 10:27:43 +0530

    multilinecomment

--- Exception Handling/4finally.py
+++ Exception Handling/4finally.py
@@ -1,20 +1,20 @@
-#finally Block:
-# It is not recommended to maintain clean up code(Resource Deallocating Code or 
-#Resource Releasing code) inside try block because there is no guarentee for the 
-#execution of every statement inside try block always.
-#It is not recommended to maintain clean up code inside except block, because if there 
-    #is no exception then except block won't be executed.
-#Hence we required some place to maintain clean up code which should be executed 
-   #always irrespective of whether exception raised or not raised and whether exception 
-#handled or not handled. Such type of best place is nothing but finally block.#
-# Hence the main purpose of finally block is to maintain clean up code
-##SYNTAX FOR FINALLY
-#try:
-#    Risky Code
-#except:
-#    Handling Code
-#finally:
-#    Cleanup code
+"""finally Block:
+ It is not recommended to maintain clean up code(Resource Deallocating Code or 
+Resource Releasing code) inside try block because there is no guarentee for the 
+execution of every statement inside try block always.
+It is not recommended to maintain clean up code inside except block, because if there 
+    is no exception then except block won't be executed.
+Hence we required some place to maintain clean up code which should be executed 
+   always irrespective of whether exception raised or not raised and whether exception 
+handled or not handled. Such type of best place is nothing but finally block.#
+ Hence the main purpose of finally block is to maintain clean up code
+SYNTAX FOR FINALLY
+try:
+    Risky Code
+except:
+    Handling Code
+finally:
+   Cleanup code """
 
 #Case-1: If there is no exception
 try:
