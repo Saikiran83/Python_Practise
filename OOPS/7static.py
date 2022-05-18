599e2e23981c573aec43186801dfcb60ea88e5bf
SAIKIRAN83 <saikithati83@gmail.com>
2022-05-18 10:44:09 +0530

    program

--- OOPS/7static.py
+++ OOPS/7static.py
@@ -1,33 +1,40 @@
-import sys 
-class Customer:
-    ''' Customer class with bank operations.. ''' 
-    bankname='SAIBANK' 
-    def __init__(self,name,balance=0.0): 
-        self.name=name 
-        self.balance=balance 
-    def deposit(self,amt): 
-        self.balance=self.balance+amt 
-        print('Balance after deposit:',self.balance) 
-    def withdraw(self,amt): 
-        if amt>self.balance: 
-            print('Insufficient Funds..cannot perform this operation') 
-            sys.exit() 
-        self.balance=self.balance-amt 
-        print('Balance after withdraw:',self.balance)  
-print('Welcome to',Customer.bankname) 
-name=input('Enter Your Name:')  
-c=Customer(name) 
-while True: 
-    print('d-Deposit \nw-Withdraw \ne-exit') 
-    option=input('Choose your option:') 
-    if option=='d' or option=='D': 
-        amt=float(input('Enter amount:')) 
-        c.deposit(amt) 
-    elif option=='w' or option=='W': 
-        amt=float(input('Enter amount:')) 
-        c.withdraw(amt) 
-    elif option=='e' or option=='E': 
-        print('Thanks for Banking') 
-        sys.exit() 
-    else: 
-        print('Invalid option..Plz choose valid option') 
+import sys
+class banking:
+    "bank operations"
+    bank_name = 'SAI BANK'
+    def __init__(self,name,balance = 0.0):
+        self.name = name 
+        self.balance = balance 
+    def deposit(self,amount):
+        self.balance = self.balance + amount 
+        print("balance after deposit is:",self.balance)
+    def withdraw(self,amount):
+        if amount>self.balance:
+            print("insufficient funds")
+            sys.exit()
+        self.balance = self.balance - amount 
+        print("balance after withdraw:",self.balance)
+print("welcome to ",banking.bank_name)
+name = input("enter your name:")
+c = banking(name)
+while True:
+    print('d-Deposit \nw-Withdraw \ne-exit')
+    option=input('Choose your option:')
+    if option == 'd' or option == 'D':
+        amount = float(input("enter amount:"))
+        c.deposit(amount)
+    elif option == 'w' or option == 'W':
+        amount = float(input("enter amount:"))
+        c.withdraw(amount)
+    elif option == 'e' or option == ' E':
+        print("thanks for banking")
+        sys.exit
+    else:
+        print("invalid option, choose correct option")
+
+
+
+
+    
+
+     
